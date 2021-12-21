fae475de477b31fa1145d4f9bc71949001c86bb0
Oliver Scott <olivercscott@gmail.com>
2021-12-21 13:00:19 +0100

    Add preference key for cleartext network policy, global
    TODO: Migration
    * This was not included in the LEGACY AOSP to Lineage migration,
      since it wasn't ready in time, and may also need a custom value migration
    
    Change-Id: I4702f7eb2ff869b92636d003179e729e0a116e2d

--- sdk/src/java/lineageos/providers/LineageSettings.java
+++ sdk/src/java/lineageos/providers/LineageSettings.java
@@ -3517,6 +3517,25 @@ public final class LineageSettings {
         /** @hide */
         public static final Validator QS_TILES_TOGGLEABLE_ON_LOCK_SCREEN_VALIDATOR =
                 sBooleanValidator;
+
+        /**
+         * Global cleartext (aka non-TLS) traffic blocking
+         *
+         * Values are:
+         * -1: StrictMode.NETWORK_POLICY_INVALID - Remove global cleartext traffic rule
+         *  0: StrictMode.NETWORK_POLICY_ACCEPT - Allow all cleartext traffic.
+         *  1: StrictMode.NETWORK_POLICY_LOG - Log all cleartext traffic.
+         *     Does not appear in logcat by default. Uses existing StrictMode logging
+         *     for cleartext network penalties.
+         *  2: StrictMode.NETWORK_POLICY_REJECT - Reject all cleartext traffic.
+         * @hide
+         */
+        public static final String CLEARTEXT_NETWORK_POLICY = "cleartext_network_policy";
+
+        /** @hide */
+        public static final Validator CLEARTEXT_NETWORK_POLICY_VALIDATOR =
+                new InclusiveIntegerRangeValidator(-1, 2);
+
         // endregion
 
         /**
@@ -3563,6 +3582,7 @@ public final class LineageSettings {
                 new ArrayMap<String, Validator>();
         static {
             VALIDATORS.put(BLUETOOTH_OFF_TIMEOUT, BLUETOOTH_OFF_TIMEOUT_VALIDATOR);
+            VALIDATORS.put(CLEARTEXT_NETWORK_POLICY, CLEARTEXT_NETWORK_POLICY_VALIDATOR);
             VALIDATORS.put(DEVICE_REBOOT_TIMEOUT, DEVICE_REBOOT_TIMEOUT_VALIDATOR);
             VALIDATORS.put(GLOBAL_VPN_APP,
                     value -> (value == null) || PACKAGE_NAME_VALIDATOR.validate(value));
