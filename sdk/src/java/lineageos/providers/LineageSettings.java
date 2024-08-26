9888c416dcd7c958843473c7bf0f461825a72ee7
Oliver Scott <olivercscott@gmail.com>
2024-08-26 15:20:45 -0400

    Add preference key for strong auth timeout, secure
    Issue: calyxos#590
    Change-Id: I91aacd5b3a0cf8a72eded332c5c5953c923066bc

--- sdk/src/java/lineageos/providers/LineageSettings.java
+++ sdk/src/java/lineageos/providers/LineageSettings.java
@@ -3084,6 +3084,15 @@ public final class LineageSettings {
         /** @hide */
         public static final Validator MAXIMUM_FAILED_PASSWORDS_FOR_WIPE_VALIDATOR =
                 sNonNegativeIntegerValidator;
+
+        /**
+         * The amount of time in milliseconds before strong auth is required
+         * @hide
+         */
+        public static final String STRONG_AUTH_TIMEOUT_MS = "strong_auth_timeout_ms";
+
+        /** @hide */
+        public static final Validator STRONG_AUTH_TIMEOUT_MS_VALIDATOR = sNonNegativeLongValidator;
         // endregion
 
         /**
@@ -3124,6 +3133,7 @@ public final class LineageSettings {
                     USER_SWITCHER_HIDDEN_WHEN_LOCKED_VALIDATOR);
             VALIDATORS.put(MAXIMUM_FAILED_PASSWORDS_FOR_WIPE,
                     MAXIMUM_FAILED_PASSWORDS_FOR_WIPE_VALIDATOR);
+            VALIDATORS.put(STRONG_AUTH_TIMEOUT_MS, STRONG_AUTH_TIMEOUT_MS_VALIDATOR);
         }
     }
 
