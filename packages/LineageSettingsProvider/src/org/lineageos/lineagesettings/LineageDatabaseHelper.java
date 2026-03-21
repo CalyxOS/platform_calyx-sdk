368528b0a7fd36617f403d1448e448e3df64861f
Torsten Grote <torsten@calyx.org>
2026-03-21 14:23:27 -0300

    Disable identity check (notification and card)
    This is a GMS feature that has onboarding in AOSP.
    
    calyxos#3460
    
    Change-Id: I8e9c808a38e19a234332a8025b5688eda6f33b6a

--- packages/LineageSettingsProvider/src/org/lineageos/lineagesettings/LineageDatabaseHelper.java
+++ packages/LineageSettingsProvider/src/org/lineageos/lineagesettings/LineageDatabaseHelper.java
@@ -214,6 +214,7 @@ public class LineageDatabaseHelper extends SQLiteOpenHelper{
     private void loadSettings(SQLiteDatabase db) {
         loadSystemSettings(db);
         loadSecureSettings(db);
+        loadIdentityCheckPromoCardShownSetting();
         // The global table only exists for the 'owner' user
         if (mUserHandle == UserHandle.USER_SYSTEM) {
             loadGlobalSettings(db);
@@ -325,6 +326,11 @@ public class LineageDatabaseHelper extends SQLiteOpenHelper{
         } finally {
             if (stmt != null) stmt.close();
         }
+    }
+
+    private void loadIdentityCheckPromoCardShownSetting() {
+        Settings.Secure.putInt(mContext.getContentResolver(),
+                Settings.Secure.IDENTITY_CHECK_PROMO_CARD_SHOWN, 1);
     }
 
     private void loadRestrictedNetworkingModeSetting() {
